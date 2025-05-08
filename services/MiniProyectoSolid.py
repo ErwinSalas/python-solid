c9a4cafda75b441b487a69ed2601f049a4e16f93
jonathansl17 <j.sancho.1@estudiantec.cr>
2025-05-07 20:28:05 -0600

    Terminado

--- services/MiniProyectoSolid.py
+++ services/MiniProyectoSolid.py
@@ -1,5 +1,6 @@
 from models.electronics import Monitor,Laptop
 
+#Clase de carrito de compras
 class CarritoDeCompras:
     def __init__(self):
         self.items = []
@@ -10,11 +11,14 @@ class CarritoDeCompras:
     def mostrar(self):
         for item in self.items:
             print(f"{item.__class__.__name__}: {item.marca} {item.modelo} - ${item.precio}")
+            
 
+# #Clase abstracta de compra
 class Compra:
     def realizar_compra(self, carrito: CarritoDeCompras):
         raise NotImplementedError("Debe ser implementado en otras clases o da error jajaja")
 
+# #Clase de compra de monitor que iplementa la clase abstracta para realizar la compra
 class ComprarMonitor(Compra):
     def __init__(self, monitor:Monitor):
         super().__init__()
@@ -23,7 +27,7 @@ class ComprarMonitor(Compra):
     def realizar_compra(self, carrito: CarritoDeCompras):
         carrito.agregar(self.monitor)
 
-
+# #Clase de compra de laptop que iplementa la clase abstracta para realizar la compra
 class ComprarLaptop(Compra):
     def __init__(self, laptop:Laptop):
         super().__init__()
